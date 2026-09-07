9d9bb8ee4fefdfc30b3d3ab2bd8989160bcc8910
Vasundraa <vasundraas@gmail.com>
2026-09-07 17:27:26 +0530

    Learnt variables, data types, operators, fstring

---
+++ Python/Foundations/hello.py
@@ -0,0 +1,10 @@
+# print("Hello World!")
+
+print("Name: Vasundraa\nAge: 22\nDegree: B.E.")
+
+name1 = "Vasundraa"
+age1 = 22
+degree1 = "B.E."
+name2 = input("Enter name:")
+print("Name of the student is",name2)
+print(f"Name of second student is {name2} and age of first student is {age1}")
